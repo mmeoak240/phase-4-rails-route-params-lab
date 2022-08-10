f9422efa5fa9259af8d5e0841bd5b6773ad7f900
Michael Meoak <mmeoak240@gmail.com>
2022-08-10 14:44:12 -0700

    commit tree b0b886909a2de1acc2a14fa7d5003395561b55e2

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:name]
+                 Student.by_name(params[:name])
+               else
+                 Student.all
+               end    
+    render json: students
+  end
+  
+  def student_id 
+    students = Student.all.find(params[:id])
     render json: students
   end
 
